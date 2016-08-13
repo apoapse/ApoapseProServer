779c16f1e51ec1e25df7e508aa2192500badda38
Guillaume Puyal <contact@guillaume-puyal.com>
2016-08-13 19:20:54 +0200

    Added ProducerConsumerQueue

---
+++ Apoapse.Core/ProducerConsumerQueue.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <boost\thread.hpp>
+#define _ENABLE_ATOMIC_ALIGNMENT_FIX
+#include <boost\lockfree\queue.hpp>
+#include <boost\atomic.hpp>
+
+#define DEFAULT_QUEUE_CAPACITY 16
+
+class ProducerConsumerQueue
+{
+private:
+	typedef std::function<void()> lambdaFunction;
+
+	boost::lockfree::queue<lambdaFunction*> m_queue{ DEFAULT_QUEUE_CAPACITY };
+	boost::thread_group m_threads;
+	boost::atomic<long int> m_tasksInQueueCount;
+	boost::atomic<long int> m_queueCapacity;
+	boost::condition_variable m_conditionVariable;
+	boost::mutex m_mutex;
+	const std::string m_name;
+
+public:
+	ProducerConsumerQueue(size_t, const char*, size_t queueInitialCapacity = DEFAULT_QUEUE_CAPACITY);
+	~ProducerConsumerQueue();
+
+	void Push(lambdaFunction callback);
+
+private:
+	void Consume();
+	void ResizeQueue(size_t);
+};
