2b9e6660419d4d0136f39dbe54e72e729df45db0
Guillaume Puyal <contact@guillaume-puyal.com>
2016-09-14 04:20:25 +0200

    Added ByteConverter and types header

--- Code/Apoapse.Core/ByteUtils.h
+++ Code/Apoapse.Core/ByteUtils.h
@@ -1,2 +1,15 @@
 #pragma once
 typedef unsigned char byte;
+
+struct ByteConverter
+{
+	inline static UInt32 ToUInt32(const byte bytes[], const size_t offset = 0)	// #WARNING: current implementation support little endian only
+	{
+		return *((UInt32*)(bytes + offset));
+	}
+
+	inline static std::string ToASCIIString(const byte bytes[], const size_t length, const size_t offset = 0)
+	{
+		return std::string((char*)bytes + offset, length);
+	}
+};
---
+++ Code/Apoapse.Core/Types.h
@@ -0,0 +1,4 @@
+#pragma once
+typedef unsigned int UInt32;
+typedef unsigned short int UInt16;
+typedef unsigned char UInt8;
