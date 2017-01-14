65ec25020cd29b861d5803201a64d85144b4f552
Guillaume Puyal <contact@guillaume-puyal.com>
2017-01-14 08:00:42 +0100

    Added ReadWriteMutexWrapper

---
+++ Code/Apoapse.Server/ReadWriteMutexWrapper.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <mutex>
+#include <boost/thread/shared_mutex.hpp>
+
+using read_mutex = boost::shared_lock<boost::shared_mutex>;
+using exclusive_mutex = std::lock_guard<boost::shared_mutex>;
+
+template <typename T>
+class ReadWriteMutexWrapper
+{
+	mutable boost::shared_mutex m_sharedLock;
+	mutable T m_data;
+
+public:
+	ReadWriteMutexWrapper()
+	{
+	}
+
+	ReadWriteMutexWrapper(const T& data) : m_data(data)
+	{
+	}
+
+	T& Get() const
+	{
+		return m_data;
+	}
+
+	void Set(const T& input)
+	{
+		m_data = input;
+	}
+
+
+	boost::shared_mutex& GetMutex() const
+	{
+		return m_sharedLock;
+	}
+
+	ReadWriteMutexWrapper& operator=(const T& input)
+	{
+		Set(input);
+
+		return *this;
+	}
+
+	ReadWriteMutexWrapper& operator=(const T&& input)
+	{
+		Set(input);
+
+		return *this;
+	}
+};
