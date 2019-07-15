a7d989ba63bf00018be166b3636c495031c1478a
Guillaume Puyal <contact@guillaume-puyal.com>
2019-07-15 22:59:08 +0200

    Fixed user manager database queries

--- ApoapseServerShared/UsersManager.cpp
+++ ApoapseServerShared/UsersManager.cpp
@@ -33,7 +33,7 @@ size_t UsersManager::GetConnectedUsersCount() const
 size_t UsersManager::GetRegisteredUsersCount() const
 {
 	SQLQuery query(*global->database);
-	query << SELECT << "username_hash" << FROM << "users";
+	query << SELECT << "username" << FROM << "users";
 	auto res = query.Exec();
 
 	return ((res) ? res.RowCount() : 0);
@@ -42,7 +42,7 @@ size_t UsersManager::GetRegisteredUsersCount() const
 std::shared_ptr<User> UsersManager::CreateUserObject(const Username& username, ServerConnection& connection)
 {
 	SQLQuery query(*global->database);
-	query << SELECT << "user_id" << FROM << "users" << WHERE << "username_hash" << EQUALS << username.GetRaw();
+	query << SELECT << "user_id" << FROM << "users" << WHERE << "username" << EQUALS << username.GetRaw();
 	auto res = query.Exec();
 
 	if (!res || res.RowCount() != 1)
@@ -61,7 +61,7 @@ std::shared_ptr<User> UsersManager::CreateUserObject(const Username& username, S
 bool UsersManager::LoginIsValid(const Username& username, const std::vector<byte>& password)
 {
 	SQLQuery query(*global->database);
-	query << SELECT << "password,password_salt" << FROM << "users" << WHERE << "username_hash" << EQUALS << username.GetRaw();
+	query << SELECT << "password,password_salt" << FROM << "users" << WHERE << "username" << EQUALS << username.GetRaw();
 	auto res = query.Exec();
 
 	if (!res || res.RowCount() == 0)
@@ -76,7 +76,7 @@ bool UsersManager::LoginIsValid(const Username& username, const std::vector<byte
 bool UsersManager::DoesUserExist(const Username& username) const
 {
 	SQLQuery query(*global->database);
-	query << SELECT << "username_hash" << FROM << "users" << WHERE << "username_hash" << EQUALS << username.GetRaw();
+	query << SELECT << "username" << FROM << "users" << WHERE << "username" << EQUALS << username.GetRaw();
 	auto res = query.Exec();
 
 	if (res && res.RowCount() == 1)
@@ -92,7 +92,7 @@ void UsersManager::RegisterNewUser(const Username& username, const std::vector<b
 	const auto salt = User::GenerateRandomSalt();
 
 	SQLQuery query(*global->database);
-	query << INSERT_INTO << "users" << " (username_hash, password, password_salt, is_temporary_passsword)" << VALUES << "(" << username.GetRaw() << "," << User::HashPassword(encryptedTemporaryPassword, salt) << "," << salt << "," << 1 << ")";
+	query << INSERT_INTO << "users" << " (username, password, password_salt, is_temporary_passsword)" << VALUES << "(" << username.GetRaw() << "," << User::HashPassword(encryptedTemporaryPassword, salt) << "," << salt << "," << 1 << ")";
 
 	query.Exec();
 
@@ -108,12 +108,12 @@ DbId UsersManager::SetUserIdentity(const Username& username, const std::vector<b
 
 	{
 		SQLQuery query(*global->database);
-		query << UPDATE << "users" << SET << "password=" << passwordForStorage << ",password_salt=" << salt << ",is_temporary_passsword=" << 0 << ",metadata_all=" << metadataAll.GetRawDataForDb() << WHERE "username_hash" << EQUALS << username.GetRaw();
+		query << UPDATE << "users" << SET << "password=" << passwordForStorage << ",password_salt=" << salt << ",is_temporary_passsword=" << 0 << ",metadata_all=" << metadataAll.GetRawDataForDb() << WHERE "username" << EQUALS << username.GetRaw();
 		query.Exec();
 	}
 
 	SQLQuery query(*global->database);
-	query << SELECT << "user_id" << FROM "users" << WHERE "username_hash" << EQUALS << username.GetRaw();
+	query << SELECT << "user_id" << FROM "users" << WHERE "username" << EQUALS << username.GetRaw();
 	auto res = query.Exec();
 
 	DbId dbId = res[0][0].GetInt64();
