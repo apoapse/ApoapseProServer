6e2bc915322f262904d88f9fb0bffc4f3d3b57a0
Guillaume Puyal <contact@guillaume-puyal.com>
2019-07-29 19:34:42 +0200

    Added a title to the server console on Windows

--- ServerConsole/Main.cpp
+++ ServerConsole/Main.cpp
@@ -3,12 +3,20 @@
 #include <string>
 #include "ServerEntryPoint.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 int main(int argcount, char* argv[])
 {
+#ifdef _WIN32
+	SetConsoleTitle("Apoapse Server");
+#endif
+
 	ServerMain(std::vector<std::string>(argv, argv + argcount));
 
-	std::string inputstr;
-	getline(std::cin, inputstr);
+	//std::string inputstr;
+	//getline(std::cin, inputstr);
 
 	return 0;
 }
