38c4b0e86cbee2a08e44a2f411b33b9dfa56a021
Guillaume Puyal <contact@guillaume-puyal.com>
2019-11-15 20:57:33 +0100

    Added linux stack trace

--- ApoapseServerShared/ServerEntryPoint.cpp
+++ ApoapseServerShared/ServerEntryPoint.cpp
@@ -17,8 +17,36 @@
 #include "UnitTestsManager.h"
 #endif // UNIT_TESTS
 
+// UNIX STACK TRACE
+#ifndef _WIN32
+#include <stdio.h>
+#include <execinfo.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+void handler(int sig) {
+  void *array[10];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 10);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+#endif
+
 int ServerMain(const std::vector<std::string>& launchArgs)
 {
+#ifndef _WIN32
+	signal(SIGSEGV, handler);	// Stack trace on linux
+#endif
+
+
 	// Starting global
 	ASSERT(global == nullptr);
 	global = Global::CreateGlobal();
